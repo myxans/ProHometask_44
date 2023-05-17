570101e2e265e1dab4362e51bf95976a51cb425b
myxans <mushenko1992@gmail.com>
2023-05-17 21:17:56 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const fetchAlbums = (userId) => {
+    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
+      .then((response) => response.json())
+      .then((data) => setAlbums(data))
+      .catch((error) => console.log(error));
+  };
+
+  const fetchPhotos = (albumId) => {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then((response) => response.json())
+      .then((data) => setPhotos(data))
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}
+            <button onClick={() => fetchAlbums(user.id)}>Albums</button>
+            {albums.length > 0 && (
+              <ul>
+                {albums.map((album) => (
+                  <li key={album.id}>
+                    {album.title}
+                    <button onClick={() => fetchPhotos(album.id)}>Photos</button>
+                    {photos.length > 0 && (
+                      <ul>
+                        {photos.map((photo) => (
+                          <li key={photo.id}>
+                            <img src={photo.thumbnailUrl} alt={photo.title} />
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default UserList;
